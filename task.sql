846a4ecb291563b1003119baebcd891343c6915c
Artem <artemkluenkov41@gmail.com>
2026-06-11 20:16:30 +0000

    solution

--- task.sql
+++ task.sql
@@ -6,5 +6,5 @@ CREATE INDEX ProductID ON OrderItems(ProductID);
 CREATE INDEX `Date` ON Orders(`Date`);
 CREATE INDEX OrderID ON OrderItems(OrderID);
 CREATE INDEX Price ON Products(Price);
-CREATE INDEX FirstName ON Customers(FirstName)
-CREATE INDEX LastName ON Customers(LastName)
+CREATE INDEX FirstName ON Customers(FirstName;)
+CREATE INDEX LastName ON Customers(LastName);
